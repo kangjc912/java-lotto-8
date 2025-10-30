121110d1daa78aa463388cf93b3f1d576d0cbac9
chan <djc0807285@gmail.com>
2025-10-31 03:31:42 +0900

    feat(Lotto): Lotto 클래스 유효성 검사(중복, 범위) 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,54 @@
 package lotto;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+
+        List<Integer> sortedNumbers = new ArrayList<>(numbers);
+        Collections.sort(sortedNumbers);
+        this.numbers = sortedNumbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+
+        validateDuplicate(numbers);
+        validateRange(numbers);
     }
 
-    // TODO: 추가 기능 구현
+
+    //중복여부 테스트
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if(uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 중복된 숫자");
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        for(Integer number : numbers){
+            checkRange(number);
+        }
+    }
+
+
+    private void checkRange(int number) {
+        if(number<1 || number>45) {
+            throw new IllegalArgumentException("[ERROR] 범위 외의 숫자");
+        }
+    }
+
+
+
 }
